6877cd6b096c8b6fff37b5ef35b5b2f3e344e977
Jeremy Wood <js.wood206@gmail.com>
2021-03-17 22:20:48 -0700

    commit tree 3fe9b0fbec0b46d65c462bd7a7ead0d6569fd0d0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your functions here
+function map(arr, func) {
+    let newArray = [];
+    for ( let i = 0; i < arr.length; i++) {
+        newArray.push(func(arr[i]));
+    }
+    return newArray;
+}
+
+function reduce(arr, func, memo ) {
+    if (!memo) {
+        memo = arr[0];
+        arr = arr.slice(1);
+    }
+    for (let i = 0; i < arr.length; i++) {
+        memo = func(arr[i], memo);
+    }
+    return memo;
+    
+}
+
